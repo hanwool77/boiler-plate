9d3b25cfcebbdce58a61347c79eacc47a277c280
Hanwool <wjsgksdnf11@naver.com>
2020-08-07 15:03:13 +0900

    처음 저장소에 올림

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://hanwool:wjsgks11@boilerplate.z8mun.mongodb.net/boilerplate?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Heo World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
